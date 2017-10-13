5aa86d129aedb80f33d9d163873837d2297c9470
AlanGuo1993 <64.uestc@gmail.com>
2017-10-13 02:34:15 -0700

    finish game board generation

--- src/Board.java
+++ src/Board.java
@@ -1,20 +1,68 @@
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+
+import java.util.List;
+
 /**
  * Created by GA on 10/13/17.
  */
 public class Board {
+    public int numOfRows;
+    public int numOfColumns;
     public int[][] cells;
     public int numOfUnsearchedCells;
+    public int numOfMines;
+    public List<String> mineCoordinateList;
 
-    public Board(int m, int n, int numOfMines) {
-        cells = new int[m][n];
-        numOfUnsearchedCells = m * n;
+    public Board(int m, int n, int numOfMines, List<String> mineCoordinateList) {
+        numOfRows = m;
+        numOfColumns = n;
+        this.numOfMines = numOfMines;
+        cells = new int[numOfRows][numOfColumns];
+        numOfUnsearchedCells = numOfRows * numOfColumns;
+        this.mineCoordinateList = mineCoordinateList;
+        putMines();
+    }
 
+    private void putMines() {
+        for(String singleMine : mineCoordinateList) {
+            String[] singleMineCoordinate = singleMine.split(" ");
+
+            //(x, y) coordinate
+            int x = Integer.parseInt(singleMineCoordinate[0]);
+            int y = Integer.parseInt(singleMineCoordinate[1]);
+
+//            if(x > numOfRows - 1 || y > numOfColumns - 1 || x < 0 || y < 0) {
+//                Exception exception = new Exception("mine should be placed in bound.");
+//                return;
+//            }
+            cells[x][y] = -1;
+
+            if(x - 1 >= 0 && y - 1 >= 0 && cells[x - 1][y - 1] != -1)
+                ++cells[x - 1][y - 1];
+            if(x >= 0 && y - 1 >= 0 && cells[x][y - 1] != -1)
+                ++cells[x][y - 1];
+            if(x + 1 <= numOfRows - 1 && y - 1 >= 0 && cells[x + 1][y - 1] != -1)
+                ++cells[x + 1][y - 1];
+            if(x - 1 >= 0 && y >= 0 && cells[x - 1][y] != -1)
+                ++cells[x - 1][y];
+            if(x + 1 <= numOfRows -1 && y >= 0 && cells[x + 1][y] != -1)
+                ++cells[x + 1][y];
+            if(x - 1 >= 0 && y + 1 <= numOfColumns - 1 && cells[x - 1][y + 1] != -1)
+                ++cells[x - 1][y + 1];
+            if(x >= 0 && y + 1 <= numOfColumns - 1 && cells[x][y + 1] != -1)
+                ++cells[x][y + 1];
+            if(x + 1 <= numOfRows - 1 && y + 1 <= numOfColumns - 1 && cells[x + 1][y + 1] != -1)
+                ++cells[x + 1][y + 1];
+        }
     }
 
     public void print() {
         for(int[] row : cells) {
-            for(int cell : row) {
-                System.out.print(cell + " ");
+            for(int cellValue : row) {
+                if(cellValue == -1)
+                    System.out.print("*" + " ");
+                else
+                    System.out.print(cellValue + " ");
             }
             System.out.println();
         }
--- src/Launcher.java
+++ src/Launcher.java
@@ -8,14 +8,18 @@ import java.util.Scanner;
 public class Launcher {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int totalNumOfMines = scanner.nextInt();
-        List<String> mineCoordinateList = new ArrayList<>();
         System.out.print("Enter the number of mines: ");
+        int totalNumOfMines = Integer.parseInt(scanner.nextLine());
+        System.out.print("Enter the number of rows: ");
+        int numOfRows = Integer.parseInt(scanner.nextLine());
+        System.out.print("Enter the number of columns: ");
+        int numOfColumns = Integer.parseInt(scanner.nextLine());
+        List<String> mineCoordinateList = new ArrayList<>();
+        System.out.println("Enter coordinates for mines (x and y split by single space): ");
         for(int i = 0; i < totalNumOfMines; i ++) {
-
+            mineCoordinateList.add(scanner.nextLine());
         }
-        String mineCoordinate = scanner.nextLine();
-        Board board = new Board(5, 5, 0);
+        Board board = new Board(numOfRows, numOfColumns, totalNumOfMines, mineCoordinateList);
         board.print();
     }
 }
